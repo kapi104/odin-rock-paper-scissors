48b310c6fe22bd5720eec61cd07cf91085264e0d
kapi104 <skorupa7979@gmail.com>
2022-07-17 18:59:55 +0200

    added getComputerChoice function

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let number = Math.floor(Math.random() * 3);
+    if (number === 0) return 'rock';
+    if (number === 1) return 'paper';
+    if (number === 2) return 'scissors';
+}
+
+
+console.log(getComputerChoice())
