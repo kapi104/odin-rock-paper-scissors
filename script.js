254638528ce7a117488708c505dc47bf202a968e
kapi104 <skorupa7979@gmail.com>
2022-07-17 20:36:05 +0200

    added play and compare functions, added start button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     if (number === 0) return 'rock';
@@ -6,17 +10,37 @@ function getComputerChoice() {
 }
 
 function playRound(playerChoice, computerChoice) {
-    if (playerChoice == computerChoice) {
-        return `it's a draw`
-    } else if (playerChoice === `paper` && computerChoice === `scissors` ||
-                playerChoice === `scissors` && computerChoice === `rock` ||
-                playerChoice === `rock` && computerChoice === `paper`) {
-        return `you lose ${computerChoice} beats ${playerChoice}`;
-    } else {
-        return `you won ${playerChoice} beats ${computerChoice}`
+    
+        if (playerChoice == computerChoice) {
+            return `it's a draw. You have ${playerPoints} and computer have ${computerPoints}`;
+        } else if (playerChoice === `paper` && computerChoice === `scissors` ||
+                    playerChoice === `scissors` && computerChoice === `rock` ||
+                    playerChoice === `rock` && computerChoice === `paper`) {
+            computerPoints = computerPoints + 1;
+            return `You lose ${computerChoice} beats ${playerChoice}. You have ${playerPoints} and computer have ${computerPoints}`;
+        } else {
+            playerPoints = playerPoints + 1;
+            return `You won ${playerChoice} beats ${computerChoice}. You have ${playerPoints} and computer have ${computerPoints}`;
+        }
+}
+
+function compare() {
+    if (playerPoints == computerPoints) return `it's a draw. You both had ${playerPoints}`;
+    else if (playerPoints > computerPoints) return `You won with ${playerPoints} points. computer had only ${computerPoints}`;
+    else return `You lose with ${playerPoints} points. computer had ${computerPoints}`; 
+}
+
+function play() {
+    console.clear;
+    for (i=0; i<=5; i++) {
+        if (i < 5) {
+            let computerSelection = getComputerChoice();
+            let playerSelection = prompt(`rock, paper or scissors`).toLowerCase();
+            console.log(playRound(playerSelection, computerSelection))
+        } else {
+            console.log(compare())
+        }
     }
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt(`rock, paper or scissors`).toLowerCase();
-console.log(playRound(playerSelection, computerSelection))
+
