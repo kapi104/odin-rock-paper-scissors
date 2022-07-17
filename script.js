8916af1b808efc5a64cfdeae5a0db249cbcfdb0f
kapi104 <skorupa7979@gmail.com>
2022-07-17 19:33:02 +0200

    added playRound function

--- script.js
+++ script.js
@@ -5,5 +5,18 @@ function getComputerChoice() {
     if (number === 2) return 'scissors';
 }
 
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        return `it's a draw`
+    } else if (playerChoice === `paper` && computerChoice === `scissors` ||
+                playerChoice === `scissors` && computerChoice === `rock` ||
+                playerChoice === `rock` && computerChoice === `paper`) {
+        return `you lose ${computerChoice} beats ${playerChoice}`;
+    } else {
+        return `you won ${playerChoice} beats ${computerChoice}`
+    }
+}
 
-console.log(getComputerChoice())
+let computerSelection = getComputerChoice();
+let playerSelection = prompt(`rock, paper or scissors`).toLowerCase();
+console.log(playRound(playerSelection, computerSelection))
